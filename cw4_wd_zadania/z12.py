f32d36ac30d7624f88c3c745b702708d5b7cc091
KamilMarkusz96 <59959219+KamilMarkusz96@users.noreply.github.com>
2020-04-26 16:59:14 +0200

    Delete z12.py

--- cw4_wd_zadania/z12.py
+++
@@ -1,8 +0,0 @@
-def miesiace():
-    kalendarz=['styczen','luty','marzec','kwiecien','maj','czerwiec','lipiec','sierpien','wrzesien','pazdziernik','listopad','grudzien']
-    for i in range (0,12):
-        yield kalendarz[i]
-
-rok=miesiace()
-for i in range(0, 12):
-    print(next(rok))
